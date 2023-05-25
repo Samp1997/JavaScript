bc8a34358c1a20bd0695dae654baab8a09d8e436
Samp1997 <sampiazza2016@gmail.com>
2023-05-24 19:12:05 -0700

    Java Assignment

---
+++ JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Hello, World!")
+//WINDOW ALERT
+
+
+
+//This is the concatenate section: 
+var sent = sent1 = "This is the beginning of of the string" , sent2 = "and this is the end of the string";
+document.write(sent1 + sent2) 
+
+//Expression section
+var age = 50;
+var newAge = age;
+
+var message = "newAge = " + newAge;
+document.write(message);

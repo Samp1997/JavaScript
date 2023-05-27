20b1e4cafcdb10663a0c551e263be4d03c461a69
Samp1997 <sampiazza2016@gmail.com>
2023-05-27 12:13:15 -0700

    math operations

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,4 +1,41 @@
-function subtraction_Function( ) {
-    var subtraction= 5 - 2;
-    document.getElementById("Math") .innerHTML = "5 - 2 = " + subtraction;
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math") .innerHTML ="5 - 2 = " + Subtraction;
 }
+//multiply
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("Math") .innerHTML = "6 x 8 = " + simple_Math;
+}
+//divide
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("Math") .innerHTML = "48 / 6 = " + simple_Math;
+
+}
+//oder of opertaion
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 - 5;
+    document. getElementById("Math") .innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math
+}
+
+function modules_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math") .innerHTML = "when you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("Math") .innerHTML = -x;
+}
+
+var x = 5;
+x++;
+document.write(x);
+
+var X = 5.25;
+X--;
+document.write(X);
+
+window.alert(Math.random)() * 100);
+//Not displaying in chrome 

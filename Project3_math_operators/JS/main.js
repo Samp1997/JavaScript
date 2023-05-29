7a18df35532200d6c930127d4dfddca11d1e6380
Samp1997 <sampiazza2016@gmail.com>
2023-05-29 15:29:23 -0700

    math changes

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,32 +1,32 @@
-function subtraction_Function() {
+function subtractionFunction_() {
     var Subtraction = 5 - 2;
-    document.getElementById("Math") .innerHTML ="5 - 2 = " + Subtraction;
+    document.getElementById("sub") .innerHTML ="5 - 2 = " + Subtraction;
 }
 //multiply
 function multiplication() {
     var simple_Math = 6 * 8;
-    document.getElementById("Math") .innerHTML = "6 x 8 = " + simple_Math;
+    document.getElementById("mult") .innerHTML = "6 x 8 = " + simple_Math;
 }
 //divide
 function division() {
     var simple_Math = 48 / 6;
-    document.getElementById("Math") .innerHTML = "48 / 6 = " + simple_Math;
+    document.getElementById("div") .innerHTML = "48 / 6 = " + simple_Math;
 
 }
 //oder of opertaion
 function more_Math() {
     var simple_Math = (1 + 2) * 10 / 2 - 5;
-    document. getElementById("Math") .innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math
+    document. getElementById("div2") .innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math
 }
 
 function modules_Operator() {
     var simple_Math = 25 % 6;
-    document.getElementById("Math") .innerHTML = "when you divide 25 by 6 you have a remainder of: " + simple_Math;
+    document.getElementById("mod") .innerHTML = "when you divide 25 by 6 you have a remainder of: " + simple_Math;
 }
 
 function negation_Operator() {
     var x = 10;
-    document.getElementById("Math") .innerHTML = -x;
+    document.getElementById("operator") .innerHTML = -x;
 }
 
 var x = 5;
@@ -37,5 +37,5 @@ var X = 5.25;
 X--;
 document.write(X);
 
-window.alert(Math.random)() * 100);
+window.alert(Math.random)() * 10 ;
 //Not displaying in chrome 

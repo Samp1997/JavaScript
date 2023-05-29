baae362b6b8296b04974d83006a02e91df79e45e
Samp1997 <sampiazza2016@gmail.com>
2023-05-29 16:47:08 -0700

    dictionary java

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,10 +1,13 @@
+//Dictionary
 function my_Dictionary() {
     var Animal = {
-        Species: "Dog",
-        Color: "Black"
-        Breed: "Labrador",
-        Age: 5,
-        sound:"Bark!"
-    };
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        sound:"Bark!",
+    }; //what var is being dispalyed
     document.getElementById("Dictionary") .innerHTML = Animal.sound;
 }
+//Delete Statement
+delete Animal.Breed
